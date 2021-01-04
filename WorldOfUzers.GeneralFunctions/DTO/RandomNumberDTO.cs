bed570214ec2b28c9fc3fbd33f262e02a27be5f9
pedroaz <pedro.azvm@gmail.com>
2021-01-04 17:47:22 -0300

    Trying to add table storage support

---
+++ WorldOfUzers.GeneralFunctions/DTO/RandomNumberDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldOfUzers.GeneralFunctions.DTO
+{
+    public class RandomNumberDTO
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTime Time { get; set; }
+        public int RandomValue { get; set; }
+
+        public RandomNumberDTO(int value)
+        {
+            PartitionKey = "RandomValue";
+            RandomValue = value;
+            RowKey = value.ToString();
+            Time = DateTime.Now;
+        }
+    }
+}
